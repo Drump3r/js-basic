c122df8ee1859bafc3110e6cce26c00343064206
Brian <jacklevybrsg@gmail.com>
2021-04-23 08:40:23 -0500

    Variables, Funciones y Sintaxis

---
+++ valores.js
@@ -0,0 +1,14 @@
+//********** Primitivos */
+
+40 // number
+"Brayan Sanchez" // String
+
+true //Boolean
+false
+
+null //no defined
+undefined 
+
+//*************Objetos */
+[1,2,3] //Array
+{Nombre: "Diego"} //Objeto
