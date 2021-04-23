f76335bd80845e75ed667f1c17dd4083c213499b
Brian <jacklevybrsg@gmail.com>
2021-04-23 10:52:05 -0500

    Arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var frutas= ["Peach", "Apple", "Banana", "Pear"];
+
+console.log(frutas);
+
+console.log(frutas.length);
+
+console.log(frutas[0]);
+
+console.log(frutas[2]);
+
+var masFrutas = frutas.push("Uvas"); //Agrega elemento al final y devuelve el nuelo length del array
+
+frutas.pop("Uvas"); //Remueve el elemento del Array y devuelve el nuevo length del array
+
+frutas.unshift("Uvas"); //Agrega elemento 
+
+frutas.shift("Uvas");
+
+frutas.indexOf("Uvas");
