3c2d4f9f7af60b09af915f623936323e25a13da3
Brian <jacklevybrsg@gmail.com>
2021-04-23 10:06:54 -0500

    Condicionales: If, Else, else if

---
+++ if.js
@@ -0,0 +1,16 @@
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Puedes votar, será tu 1ra votación");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aun no puedes votar");
+}
+
+
+condition ? true : false;
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí soy un uno" : "No, no soy uno"
