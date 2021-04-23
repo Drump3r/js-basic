c577e5fb86bee99c58a0fe3b4a2fb167b900e8b9
Brian <jacklevybrsg@gmail.com>
2021-04-23 08:47:22 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Jesus";
+
+// declarar
+var edad;
+// inicializar
+edad =30;
+
+edad;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30,
+}
+
+persona;
