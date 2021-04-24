238f6ee9fa20b21dc8ebd46e87c92650fe663acd
Brian <jacklevybrsg@gmail.com>
2021-04-24 17:19:09 -0500

    Métodos de recorridos de Arrays

--- arrays.js
+++ arrays.js
@@ -17,3 +17,23 @@ frutas.unshift("Uvas"); //Agrega elemento
 frutas.shift("Uvas");
 
 frutas.indexOf("Uvas");
+
+//*******Métodos de recorridos de Arrays */
+
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Tv", costo: 2500},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Lapto", costo: 20000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700},
+];
+
+var articulosFiltrados = articulos.filter(function(articulo){
+    return articulo.costo <= 500;
+});
+
+var nombreArticulos = articulos.map(function(articulo){
+    return articulo.nombre;
+});
