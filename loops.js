15e90a7621e5e3e7c09360e93545ed6b41a6329d
Brian <jacklevybrsg@gmail.com>
2021-04-24 17:30:30 -0500

    Recorriendo Arrays con .find(), .forEach() y .some()

--- loops.js
+++ loops.js
@@ -21,3 +21,32 @@ while (estudiantes.length > 0) {
     var estudiante = estudiantes.shift();
     saludarEstudiantes(estudiante);
 }
+
+
+/********* Recorriendo Arrays con .find(), .forEach() y .some() */
+
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Tv", costo: 2500},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Lapto", costo: 20000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700},
+];
+
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop"
+});
+
+console.log(encuentraArticulo);
+
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+});
+
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.costo <= 700;
+});
+
+console.log(articulosBaratos);
