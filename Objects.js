3b251c61d42487150f8fc79c309233abaf2fcc7f
Brian <jacklevybrsg@gmail.com>
2021-04-24 17:02:38 -0500

    Objects: Función constructora

--- Objects.js
+++ Objects.js
@@ -1,3 +1,12 @@
+function auto(marca, modelo, annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Tesla", "Model 3", 2020);
+var autoNuevo2 = new auto("Toyota", "Corolla", 2020);
+
 var miAuto = {
     marca: "Toyota",
     modelo: "Corolla",
